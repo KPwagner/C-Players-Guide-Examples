15772116cce9390b60a1a21fb8a9c82813169cb8
Kyle Wagner <kyle.p.wagner@gmail.com>
2018-10-14 11:03:13 -0600

    add FizzBuzz challenge to Looping/Program.cs

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -12,6 +12,7 @@ namespace Loops
         {
             /*
                 1. Print-a-Pyramid
+
                 The challenge is to print the following pattern while using only one "*" character in the code:
                     *
                    ***
@@ -19,6 +20,7 @@ namespace Loops
                  *******
                 *********
             */
+
             // Outermost for loop to iterate over the rows
             for (int row = 0; row < 5; row++)
             {
@@ -38,7 +40,42 @@ namespace Loops
                 Console.WriteLine();
             }
 
+            /*
+                2. FizzBuzz
 
+                The challenge is to print out all number from 1 to 100 with the following exceptions:
+                1) If the number is divisible by 3, print "Fizz" instead of the number.
+                2) If the number is divisible by 5, print "Buzz" instead.
+                3) If the number is divisbible by 3 and 5, print "FizzBuzz".
+            */
+
+            // Loops through number 1 to 100
+            for (int num = 1; num < 101; num++)
+            {
+                // Checks if remainder is 0 when dividing number by 3
+                if (num % 3 == 0)
+                {
+                    // Checks if remainder is 0 when dividing number by 5
+                    if (num % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    // Else is necessary so only one line is written
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                // If the number is not evenly divisible by 3 or 5, the number is prints
+                else 
+                {
+                    Console.WriteLine(num);
+                }
+            }
         }
     }
 }
