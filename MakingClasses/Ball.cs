b1342f222a03b5194dc9b2a8680b02c499de888a
Kyle Wagner <kyle.p.wagner@gmail.com>
2018-10-22 23:52:52 -0500

    add challenge 2 of 2, create a Ball class

---
+++ MakingClasses/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace MakingClasses
+{
+    public class Ball
+    {
+        private int size;
+        private Color color;
+        private int numThrows;
+
+        // Basic constructor requiring only size, and defaulting numThrows to 0
+        public Ball(int size)
+        {
+            this.size = size;
+            this.numThrows = 0;
+        }
+
+        // Constructor requiring size and a color object (color class defined elsewhere in this solution)
+        public Ball(int size, Color color)
+        {
+            this.size = size;
+            this.color = color;
+            this.numThrows = 0;
+        }
+
+        // Pop method changes the size of the ball to 0
+        public void Pop()
+        {
+            this.size = 0;
+        }
+
+        // Throw method add one to numThrows
+        public void Throw()
+        {
+            if(this.size != 0)
+            {
+                this.numThrows += 1;
+            }
+        }
+
+        // Getter methods
+        public int GetSize()
+        {
+            return this.size;
+        }
+
+        public int GetThrows()
+        {
+            return this.numThrows;
+        }
+    }
+}
--- MakingClasses/Program.cs
+++ MakingClasses/Program.cs
@@ -25,6 +25,39 @@ namespace MakingClasses
                               $"\nBLUE\t\t{purple.GetBlue()}" +
                               $"\nALPHA\t\t{purple.GetAlpha()}" +
                               $"\nGRAYSCALE\t{purple.GetGrayscale()}");
+
+            // Running some code from the Ball class
+
+            // Creating a ball object
+            Ball purpleBall = new Ball(14, purple);
+            Console.WriteLine($"\n\nThe ball purpleBall looks like this:" +
+                              $"\nSIZE\t\t{purpleBall.GetSize()}" +
+                              $"\nTHROWS\t\t{purpleBall.GetThrows()}");
+
+            purpleBall.Throw();
+            purpleBall.Throw();
+            purpleBall.Pop();
+            purpleBall.Throw();
+
+            Console.WriteLine($"\nAfter throwing purpleBall a few times then poping it, it looks like this:" +
+                              $"\nSIZE\t\t{purpleBall.GetSize()}" +
+                              $"\nTHROWS\t\t{purpleBall.GetThrows()}");
+
+            // Creating another ball object
+            Ball redBall = new Ball(16, new Color(255, 0, 0));
+            Console.WriteLine($"\n\nThe ball redBall looks like this:" +
+                              $"\nSIZE\t\t{redBall.GetSize()}" +
+                              $"\nTHROWS\t\t{redBall.GetThrows()}");
+
+            redBall.Throw();
+            for (int i = 0; i < 4; i++)
+            {
+                redBall.Throw();
+            }
+
+            Console.WriteLine($"\nAfter throwing redBall a few times, it looks like this:" +
+                              $"\nSIZE\t\t{redBall.GetSize()}" +
+                              $"\nTHROWS\t\t{redBall.GetThrows()}");
         }
     }
 }
